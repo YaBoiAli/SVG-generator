6c9222e2b2ad34d5b684b4f251bb572a035c1b1c
Ali Nawab <alinawab9830@gmail.com>
2023-06-17 20:11:48 -0500

    Touch ups

---
+++ README.md
@@ -0,0 +1,57 @@
+# SVG Logo Generator
+
+This command-line application generates an SVG logo based on user input. It allows you to specify text, text color, shape, and shape color. The generated SVG file can be opened in a browser to view the logo.
+
+## Usage
+
+1. Clone the repository:
+
+   ```git clone https://github.com/your-username/svg-logo-generator.git```
+
+2. Navigate to the project directory:
+   
+   ```
+   cd svg-logo-generator
+   ```
+
+3. Install the dependencies:
+
+```
+    npm install
+```
+
+5. Run the application:
+    
+    ```
+
+    node index.js
+
+    ```
+
+7. Follow the prompts to enter the text, text color, shape, and shape color.
+
+8. After entering all the required information, the application will generate an SVG file named logo.svg in the examples folder. The file will automatically open in Visual Studio Code.
+
+9. Open the logo.svg file in a browser to view the generated logo.
+
+## Requirements
+
+    Node.js (version 12 or above)
+    npm (Node Package Manager)
+## Dependencies
+
+    inquirer
+    open
+## License
+
+This project is licensed under the MIT License.
+
+In the README file, the "Usage" section provides step-by-step instructions on how to use the program. The "Example Logo" section includes an `<img>` tag where you can later add the path to your generated logo image.
+
+Remember to replace `your-username` in the repository URL with your actual GitHub username.
+
+After generating the logo image, you can update the README.md file with the correct path to the image, like this:
+
+## Example Logo
+
+![Generated Logo](./examples/circle.svg)
---
+++ index.js
@@ -0,0 +1,108 @@
+const inquirer = require('inquirer');
+const { Triangle, Circle, Square } = require('./lib/shapes');
+const { exec } = require('child_process');
+
+// Prompt the user for text input
+function promptText() {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'text',
+      message: 'Enter text (up to 3 characters):',
+      validate: (input) => {
+        if (input.length <= 3) {
+          return true;
+        }
+        return 'Please enter up to 3 characters.';
+      },
+    },
+  ]);
+}
+
+// Prompt the user for the text color
+function promptTextColor() {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'color',
+      message: 'Enter text color (keyword or hexadecimal):',
+    },
+  ]);
+}
+
+// Prompt the user for the shape selection
+function promptShape() {
+  return inquirer.prompt([
+    {
+      type: 'list',
+      name: 'shape',
+      message: 'Choose a shape:',
+      choices: ['Circle', 'Triangle', 'Square'],
+    },
+  ]);
+}
+
+// Prompt the user for the shape color
+function promptShapeColor() {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'color',
+      message: 'Enter shape color (keyword or hexadecimal):',
+    },
+  ]);
+}
+
+let shapeName = "";
+
+let shapePos = "";
+
+// Generate the SVG file
+function generateSvgFile(text, textColor, shape, shapeColor) {
+  let shapeInstance;
+  switch (shape) {
+    case 'Circle':
+      shapeInstance = new Circle(shapeColor);
+      shapeName = "circle";
+      shapePos = "x='100' y='120'";
+
+      break;
+    case 'Triangle':
+      shapeInstance = new Triangle(shapeColor);
+      shapeName = "triangle";
+      shapePos = "x='155' y='140'";
+      break;
+    case 'Square':
+      shapeInstance = new Square(shapeColor);
+      shapeName = "square";
+      shapePos = "x='100' y='120'";
+      break;
+    default:
+      console.error('Invalid shape selection');
+      return;
+  }
+
+  const svg = `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${shapeInstance.render()}<text ${shapePos} font-size="60" font-family="Times New Roman" text-anchor="middle" fill="${textColor}">${text}</text>`;
+  const filePath = `./examples/${shapeName}.svg`;
+  require('fs').writeFileSync(filePath, svg);
+  console.log('Generated logo.svg');
+
+  // Open the SVG file in Visual Studio Code
+  exec(`code -a "${filePath}"`, (error) => {
+    if (error) {
+      console.error('Error opening file in Visual Studio Code:', error);
+    }
+  });
+}
+
+// Run the application
+async function runApplication() {
+  const { text } = await promptText();
+  const { color: textColor } = await promptTextColor();
+  const { shape } = await promptShape();
+  const { color: shapeColor } = await promptShapeColor();
+
+  generateSvgFile(text, textColor, shape, shapeColor);
+}
+
+runApplication();
